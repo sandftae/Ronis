56556312cc3d3068e5679e43479c5e1d9a32716f
sandftae <sandftae@gmail.com>
2018-10-17 20:46:10 +0300

    Update model.class.php

--- ronis/lib/model.class.php
+++ ronis/lib/model.class.php
@@ -1,16 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 111
- * Date: 12.02.2018
- * Time: 04:07
- */
 
-class Model extends App{
-    /*
-     * Обязательно перенести все важные конструкторы сюда и подключения
-     *
-     */
-
-
-}
+class Model extends App{ }
